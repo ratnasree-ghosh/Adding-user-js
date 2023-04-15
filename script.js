88274e025db0375673316d6f673d8e0e5e8acc05
Ratnasree Ghosh <86732136+ratnasree-ghosh@users.noreply.github.com>
2023-04-15 14:46:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var user_name = document.getElementById("username").value;
+var user_profession = document.getElementById("profession").value;
+var user_age = document.getElementById("age").value;
+
+function addUser(){
+    console.log(user_name, user_age, user_profession);
+}
+
